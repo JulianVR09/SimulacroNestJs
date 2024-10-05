db630089831a2edd9951971524d2bc309f8ae73e
JulianVR09 <julianvillacis.02@gmail.com>
2024-10-04 19:52:13 -0500

    feat(Product): Create folder of products with service, controller, module, dto and schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ProductModule
+  ],
+  controllers: [ProductController],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/product/product.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schema/product.schema';
+import { ProductController } from './product.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
+  providers: [ProductService],
+  controllers: [ProductController],
+ 
+})
+export class ProductModule {}
